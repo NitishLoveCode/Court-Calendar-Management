ecb0aa9de23c242bf03cf872b5972e42c0595d2f
Nitish kumar <nitishlovecode@gmail.com>
2024-12-08 12:56:29 +0530

    User Entity created.

--- src/main/java/com/courtCalendarManagement/courtCalendarManagement/Entitys/UserEntity.java
+++ src/main/java/com/courtCalendarManagement/courtCalendarManagement/Entitys/UserEntity.java
@@ -1,10 +1,14 @@
 package com.courtCalendarManagement.courtCalendarManagement.Entitys;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -24,5 +28,10 @@ public class UserEntity {
     String lastName;
 
     String email;
+
     String password;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "user_role_id", referencedColumnName = "id") // Foreign key to UserRole
+    UserRole userRole;
 }
